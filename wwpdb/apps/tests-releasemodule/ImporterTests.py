def093293e58c2a93d0ecf56dcdbef57897250cd
Ezra Peisach <ezra.peisach@rcsb.org>
2019-01-29 07:07:39 -0500

    Test import of MinitorCitationUpdate

--- wwpdb/apps/tests-releasemodule/ImporterTests.py
+++ wwpdb/apps/tests-releasemodule/ImporterTests.py
@@ -53,6 +53,8 @@ from wwpdb.apps.releasemodule.webapp.ReleaseWebApp_v2 import ReleaseWebApp
 from wwpdb.apps.releasemodule.citation.CitationFinder import CitationFinder
 from wwpdb.apps.releasemodule.citation.SearchMP import SearchMP
 from wwpdb.apps.releasemodule.citation.FetchMP import FetchMP
+from wwpdb.apps.releasemodule.citation.MonitorCitationUpdate import MonitorCitationUpdate
+
 
 class ImportTests(unittest.TestCase):
     def setUp(self):
