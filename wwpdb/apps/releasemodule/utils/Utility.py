f7f711755413ce5c999bc2309b2a50d310effc2c
Ezra Peisach <ezra.peisach@rcsb.org>
2022-11-14 17:00:23 -0500

    Assume sh syntax for script running

--- wwpdb/apps/releasemodule/utils/Utility.py
+++ wwpdb/apps/releasemodule/utils/Utility.py
@@ -181,10 +181,7 @@ def RunScript(path, script, log):
     """Run script command
     """
     cmd = 'cd ' + path + '; chmod 755 ' + script \
-        + "; echo $SHELL " \
-        + '; ./' + script + ' >& ' + log
-    print("XXXX cmd is ", cmd)
-    print("XXXX log is ", log)
+        + '; ./' + script + ' > ' + log + " 2>&1"
     os.system(cmd)
 
 
